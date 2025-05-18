16524252bead738e864353f591782d2d2e0e54f9
kei <otaki0413.it@gmail.com>
2025-05-18 12:14:27 +0900

    ✨ 検索時のデバウンス機能の実装

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Message } from "./components/Common/Message";
 import { MainLayout } from "./components/Layout/MainLayout";
 import { MovieList } from "./components/MovieList/MovieList";
 import { Search } from "./components/Search/Search";
+import { useDebounce } from "./hooks/useDebounce";
 import { useMovies } from "./hooks/useMovies";
 
 function App() {
   const [keyword, setKeyword] = useState("");
+  const debouncedKeyword = useDebounce(keyword, 500);
   const [year, setYear] = useState("");
-  const { movies, isLoading, error, loadMore } = useMovies(keyword, year);
+  const { movies, isLoading, error, loadMore } = useMovies(
+    debouncedKeyword,
+    year
+  );
 
   const renderContent = () => {
     if (isLoading) return <Message>Loading...</Message>;
     if (error) return <Message>{error}</Message>;
-    if (!keyword)
+    if (!debouncedKeyword)
       return <Message>検索フォームからキーワードを入力してください</Message>;
     return <MovieList movies={movies} loadMore={loadMore} />;
   };
